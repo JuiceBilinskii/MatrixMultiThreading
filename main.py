5872401087378e0ef6a99b410abfecc3bd603d5a
bilin <bilinskij2001@gmail.com>
2021-10-07 04:39:08 +0300

    Reworked multithreading

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ import time
 import threading
 
 
+def create_squared_matrix(size: int, low: float, high: float) -> np.ndarray:
+    return np.random.uniform(low, high, size=(size, size))
+
+
 def convert_rows(matrix, fd, size, start_row, end_row):
     for i in range(start_row, end_row):
         scale = matrix[i, fd] / matrix[fd, fd]
         for j in range(fd, size):
             matrix[i, j] = matrix[fd, j] * scale - matrix[i, j]
-
-
-def create_squared_matrix(size: int, low: float, high: float) -> np.ndarray:
-    return np.random.uniform(low, high, size=(size, size))
 
 
 def thread_convert_matrix_to_triangular(matrix: np.ndarray) -> None:
@@ -20,7 +20,7 @@ def thread_convert_matrix_to_triangular(matrix: np.ndarray) -> None:
         start_row = fd + 1
         middle_row = (start_row + size) // 2
 
-        if start_row != middle_row:
+        if fd < 100:
             thread_a = threading.Thread(target=convert_rows, args=(matrix, fd, size, start_row, middle_row))
             thread_b = threading.Thread(target=convert_rows, args=(matrix, fd, size, middle_row, size))
 
@@ -30,9 +30,10 @@ def thread_convert_matrix_to_triangular(matrix: np.ndarray) -> None:
             thread_a.join()
             thread_b.join()
         else:
-            thread = threading.Thread(target=convert_rows, args=(matrix, fd, size, start_row, size))
-            thread.start()
-            thread.join()
+            for i in range(fd + 1, size):
+                scale = matrix[i, fd] / matrix[fd, fd]
+                for j in range(fd, size):
+                    matrix[i, j] = matrix[fd, j] * scale - matrix[i, j]
 
 
 def non_thread_convert_matrix_to_triangular(matrix: np.ndarray) -> None:
@@ -53,26 +54,23 @@ def calculate_determinant_of_triangular_matrix(triangular_matrix: np.ndarray) ->
 
 
 if __name__ == '__main__':
-    matrix_size = 150
+    matrix_size = 200
     left_scope, right_scope = -1.0, 1.0
 
-    squared_matrix_a = create_squared_matrix(matrix_size, left_scope, right_scope)
-    squared_matrix_b = squared_matrix_a.copy()
-    matrix_copy = squared_matrix_a.copy()
+    initial_matrix = create_squared_matrix(matrix_size, left_scope, right_scope)
+    matrix_a = initial_matrix.copy()
+    matrix_b = initial_matrix.copy()
 
     start_time = time.time()
-    thread_convert_matrix_to_triangular(squared_matrix_a)
+    thread_convert_matrix_to_triangular(matrix_a)
     end_time = time.time()
-
     print(f'Thread: {end_time - start_time} seconds')
 
     start_time = time.time()
-    non_thread_convert_matrix_to_triangular(squared_matrix_b)
+    non_thread_convert_matrix_to_triangular(matrix_b)
     end_time = time.time()
-
     print(f'Non-thread: {end_time - start_time} seconds')
 
-    determinant = calculate_determinant_of_triangular_matrix(squared_matrix_b)
-
-    print(f'Determinant: {determinant}')
-    print(f'Numpy determinant: {np.linalg.det(matrix_copy)}')
+    print(f'Thread Determinant: {calculate_determinant_of_triangular_matrix(matrix_a)}')
+    print(f'Non-thread Determinant: {calculate_determinant_of_triangular_matrix(matrix_b)}')
+    print(f'Numpy determinant: {np.linalg.det(initial_matrix)}')
