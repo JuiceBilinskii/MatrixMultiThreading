cae046ced2b1011c0dc300cba1edfde1d6456841
bilin <bilinskij2001@gmail.com>
2021-10-12 00:55:58 +0300

    Changed determinant calculation algorithm

--- main.py
+++ main.py
@@ -1,76 +1,67 @@
 import numpy as np
 import time
-import threading
+import multiprocessing
 
 
 def create_squared_matrix(size: int, low: float, high: float) -> np.ndarray:
     return np.random.uniform(low, high, size=(size, size))
 
 
-def convert_rows(matrix, fd, size, start_row, end_row):
-    for i in range(start_row, end_row):
-        scale = matrix[i, fd] / matrix[fd, fd]
-        for j in range(fd, size):
-            matrix[i, j] = matrix[fd, j] * scale - matrix[i, j]
+def calculate_recursive_determinant(matrix: np.ndarray) -> float:
+    if len(matrix) == 2:
+        return matrix[0, 0] * matrix[1, 1] - matrix[1, 0] * matrix[0, 1]
+
+    total = 0
+    for focus_column in range(len(matrix)):
+        copied_matrix = np.delete(matrix[1:], focus_column, 1)
+        sign = (-1) ** (focus_column % 2)
+        sub_determinant = calculate_recursive_determinant(copied_matrix)
+        total += sign * matrix[0, focus_column] * sub_determinant
+    return total
 
 
-def thread_convert_matrix_to_triangular(matrix: np.ndarray) -> None:
-    size = len(matrix)
-    for fd in range(size):
-        start_row = fd + 1
-        middle_row = (start_row + size) // 2
+def multiprocess_calculate_recursive_determinant(matrix: np.ndarray, start_column, end_column) -> float:
+    if len(matrix) == 2:
+        return matrix[0, 0] * matrix[1, 1] - matrix[1, 0] * matrix[0, 1]
 
-        if fd < 100:
-            thread_a = threading.Thread(target=convert_rows, args=(matrix, fd, size, start_row, middle_row))
-            thread_b = threading.Thread(target=convert_rows, args=(matrix, fd, size, middle_row, size))
-
-            thread_a.start()
-            thread_b.start()
-
-            thread_a.join()
-            thread_b.join()
-        else:
-            for i in range(fd + 1, size):
-                scale = matrix[i, fd] / matrix[fd, fd]
-                for j in range(fd, size):
-                    matrix[i, j] = matrix[fd, j] * scale - matrix[i, j]
+    total = 0
+    for focus_column in range(start_column, end_column):
+        copied_matrix = np.delete(matrix[1:], focus_column, 1)
+        sign = (-1) ** (focus_column % 2)
+        sub_determinant = calculate_recursive_determinant(copied_matrix)
+        total += sign * matrix[0, focus_column] * sub_determinant
+    print(total)
+    # return total
 
 
-def non_thread_convert_matrix_to_triangular(matrix: np.ndarray) -> None:
-    size = len(matrix)
-    for fd in range(size):
-        for i in range(fd + 1, size):
-            scale = matrix[i, fd] / matrix[fd, fd]
-            for j in range(fd, size):
-                matrix[i, j] = matrix[fd, j] * scale - matrix[i, j]
+def manage_multiprocess_determinant_calculation(matrix):
+    process_a = multiprocessing.Process(target=multiprocess_calculate_recursive_determinant,
+                                        args=(matrix, 0, len(matrix) // 2))
+    process_b = multiprocessing.Process(target=multiprocess_calculate_recursive_determinant,
+                                        args=(matrix, len(matrix) // 2, len(matrix)))
 
+    process_a.start()
+    process_b.start()
 
-def calculate_determinant_of_triangular_matrix(triangular_matrix: np.ndarray) -> float:
-    size = len(triangular_matrix)
-    product = 1.0
-    for i in range(size):
-        product *= triangular_matrix[i, i]
-    return product
+    process_a.join()
+    process_b.join()
 
 
 if __name__ == '__main__':
-    matrix_size = 200
+    matrix_size = 10
     left_scope, right_scope = -1.0, 1.0
 
     initial_matrix = create_squared_matrix(matrix_size, left_scope, right_scope)
-    matrix_a = initial_matrix.copy()
-    matrix_b = initial_matrix.copy()
 
     start_time = time.time()
-    thread_convert_matrix_to_triangular(matrix_a)
+    manage_multiprocess_determinant_calculation(initial_matrix)
     end_time = time.time()
-    print(f'Thread: {end_time - start_time} seconds')
+    print(f'Recursive multiprocess determinant: {end_time - start_time}')
 
     start_time = time.time()
-    non_thread_convert_matrix_to_triangular(matrix_b)
+    determinant = calculate_recursive_determinant(initial_matrix)
     end_time = time.time()
-    print(f'Non-thread: {end_time - start_time} seconds')
+    print(f'Recursive single process determinant: {end_time - start_time}')
+    print(determinant)
 
-    print(f'Thread Determinant: {calculate_determinant_of_triangular_matrix(matrix_a)}')
-    print(f'Non-thread Determinant: {calculate_determinant_of_triangular_matrix(matrix_b)}')
-    print(f'Numpy determinant: {np.linalg.det(initial_matrix)}')
+    print(np.linalg.det(initial_matrix))
